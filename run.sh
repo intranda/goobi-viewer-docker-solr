13590387ffcfd862ab6294384e99a58e156b2219
Lars Geiger <lars.geiger@intranda.com>
2026-05-29 16:35:24 +0200

    Switched from su to gosu since su forks the process instead of just executing the application

--- run.sh
+++ run.sh
@@ -7,5 +7,4 @@ groupmod -o -g "${SOLR_GID}" solr
 usermod -o -u "${SOLR_UID}" solr
 chown -R solr:solr /opt/goobiviewer /opt/solr /opt/solr-9.8.0-slim /opt/java /var/solr
 
-ROOT_PATH="$PATH"
-exec su solr -c "PATH=$ROOT_PATH solr-foreground"
+exec gosu solr solr-foreground
